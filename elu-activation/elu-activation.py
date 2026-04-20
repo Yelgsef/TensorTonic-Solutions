77ad43ad9afafa4d7500e7e9a2f69a68cdc0e0ff
Trng <trungnguyen250904@gmail.com>
2026-04-20 22:51:16 +0700

    Add solution for elu-activation

---
+++ elu-activation/elu-activation.py
@@ -0,0 +1,11 @@
+ 
+def elu(x, alpha):
+    """
+    Apply ELU activation to each element.
+    """
+    # Write code here
+    for i in range(len(x)):
+        if (x[i] <= 0):
+            x[i] = alpha * (math.exp(x[i]) - 1)
+
+    return x
